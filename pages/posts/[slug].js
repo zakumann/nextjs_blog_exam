5401b40dea46185c913de41e30dc3ac37670c0ba
zakumann <samuel742able@gmail.com>
2023-12-15 15:57:47 +0900

    section 22

---
+++ pages/posts/[slug].js
@@ -0,0 +1,36 @@
+import Head from "next/head";
+import { getPost, getSlugs } from '../../lib/posts';
+
+export async function getStaticPaths(){
+    const slugs = await getSlugs();
+    return {
+        paths: slugs.map((slug) => ({
+            params: { slug },
+        })),
+        fallback: false,
+    };
+}
+
+export async function getStaticProps({ params: { slug } }){
+    console.log('[PostPage] getStaticProps():', slug);
+    const post = await getPost(slug);
+    return {
+        props: { post },
+    };
+}
+
+export default function PostPage({ post }){
+    console.log('[PostPage] render:', post);
+    return(
+        <>
+        <Head>
+            <title>{`${post.title} - My Blog`}</title>
+        </Head>
+        <main>
+            <p>{post.date}</p>
+            <h1>{post.title}</h1>
+            <article dangerouslySetInnerHTML={{ __html: post.body }} />
+        </main>
+        </>
+    );
+}
